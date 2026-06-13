ba93753eb7264751b403d768415fad877faa2104
Igor Reis <igorreis377@gmail.com>
2026-06-13 00:39:23 -0300

    chore(mail): remove test email sending method from MailService

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -5,11 +5,4 @@ import { MailerService } from "@nestjs-modules/mailer";
 export class MailService {
     constructor(private readonly mailerService: MailerService) {}
     
-    async sendTestEmail() {
-        await this.mailerService.sendMail({
-            to: "igorresi377@gmail.com",
-            subject: "teste email",
-            html: "teste2"
-        })
-    }
 }
